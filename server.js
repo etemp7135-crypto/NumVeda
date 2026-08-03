210646aec059af739023b91caec5f0edf51d9a6f
Niravgithub2005 <niravlakhani2125@gmail.com>
2026-08-03 21:46:50 +0530

    chore: add debug route

--- server.js
+++ server.js
@@ -81,6 +81,13 @@ app.post('/api/verify-payment', (req, res) => {
 app.get('/love', (req, res) => {
   res.sendFile('love.html', { root: __dirname });
 });
+app.get('/debug', (req, res) => {
+  const fs = require('fs');
+  res.json({
+    dirname: __dirname,
+    files: fs.readdirSync(__dirname)
+  });
+});
 
 // Serve index.html for root route and all other unmatched routes (SPA fallback)
 app.use((req, res) => {
